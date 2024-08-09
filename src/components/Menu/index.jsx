13882693df6471387d3c0fddb00755f1f4496dc3
Alisson de Oliveira <72466422+alissonoliveira1@users.noreply.github.com>
2024-08-09 18:59:17 -0300

    refactor: Update CSS styles for menu mobile and apresentação mobile components

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -17,14 +17,14 @@ function Menu() {
     <div className='paiMenu'>
 
     <div className='MenuLat'>
-    <Link  to="/home"><div className='icons-menu'><House2 className='icon-menu2'  /><span className='opcoes-menu'>Home</span></div></Link>
-    <Link  to='/favoritos'><div className='icons-menu'><Like className='icon-menu2'  /><span className='opcoes-menu'>Favoritos</span></div></Link>
-    <Link to='/FilmePag'><div className='icons-menu'><Filmes className='icon-menu2'  /><span className='opcoes-menu opcoes-menu2'>Filmes</span></div></Link>
-    <Link to='/Serie'><div className='icons-menu'><Serie className='icon-menu2'   /><span className='opcoes-menu opcoes-menu2'>Series</span></div></Link>
+    <Link  to="/home"><div className='icons-menu'><span className='opcoes-menu opcoes-menu2'>Home</span></div></Link>
+    <Link  to='/favoritos'><div className='icons-menu'><span className='opcoes-menu opcoes-menu2'>Favoritos</span></div></Link>
+    <Link to='/FilmePag'><div className='icons-menu'><span className='opcoes-menu opcoes-menu2'>Filmes</span></div></Link>
+    <Link to='/Serie'><div className='icons-menu'><span className='opcoes-menu opcoes-menu2'>Series</span></div></Link>
    
 
     </div>
- <div className='icons-menu ' onClick={handleMenu} ><div>{photo.map((e,index)=>{return(<div key={index} className='combo-avatar'><div className='div-image-perfil'><img className='img-perfil' src={e.usuario} alt="avatar" /></div></div>)})}</div></div>
+ <div className='icons-menu2 ' onClick={handleMenu} ><div>{photo.map((e,index)=>{return(<div key={index} className='combo-avatar'><div className='div-image-perfil'><img className='img-perfil' src={e.usuario} alt="avatar" /></div></div>)})}</div></div>
 </div>
 
 
