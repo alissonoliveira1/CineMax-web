b668c5183d0e9eb74d8b54973a87f31c32916554
Alisson de Oliveira <72466422+alissonoliveira1@users.noreply.github.com>
2025-12-03 16:15:09 -0300

    Update video source URL in FilmePlay component

--- src/page/FilmePlay/index.jsx
+++ src/page/FilmePlay/index.jsx
@@ -3,6 +3,6 @@ import './style.css'
 export default function FilmePlay(){
     const { id } = useParams();
     return(
-        <div><iframe className="videoF"  src={` https://superflixapi.mom/filme/${id}#noLink#noEpList#transparent#color:004aad` }scrolling="no" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen="" title="filme"></iframe></div>
+        <div><iframe className="videoF"  src={` https://superflixapi.asia/filme/${id}#noLink#noEpList#transparent#color:004aad` }scrolling="no" frameborder="0" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen="" title="filme"></iframe></div>
     )
 }
